a20b156008ca686addd2d3cabf1519133b1d10d3
BenteKavsek <35726@ma-web.nl>
2022-11-24 10:29:10 +0100

    opdracht 1 let zonder haakjes

--- les-01/app.js
+++ les-01/app.js
@@ -1,14 +1,14 @@
 class App {
-    runApplication() {
-        console.log("hello world!");
 
-        let appNaam = ("hello");
-        let versienummer = (0.9);
-        let versiedatum = (24112022);
-        let autheur = ("Bente");
-        let copyright = ("Me");
-        let distributeur = ("PostNL");
-        let darkmode = (true);
+    runApplication() {
+        console.log ("hello world!");
+        let appNaam = "hello";
+        let versienummer = 0.9;
+        let versiedatum = 24112022;
+        let autheur = "Bente";
+        let copyright = "Me";
+        let distributeur = "PostNL";
+        let darkmode = true;
 
         console.log(appNaam, versienummer, versiedatum, autheur, copyright, distributeur, darkmode,);
     }
